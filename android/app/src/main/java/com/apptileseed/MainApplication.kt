a82baa3269105b0a44e7848d3793430d9ec46f9b
aman-apptile <mohammed.aman@apptile.io>
2025-09-03 13:08:12 +0530

    Update with fixes

--- android/app/src/main/java/com/apptileseed/MainApplication.kt
+++ android/app/src/main/java/com/apptileseed/MainApplication.kt
@@ -14,7 +14,7 @@ import com.facebook.react.bridge.JavaScriptExecutorFactory
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.react.modules.systeminfo.AndroidInfoHelpers
 import com.facebook.react.modules.i18nmanager.I18nUtil
 import com.facebook.react.modules.systeminfo.AndroidInfoHelpers
 import com.facebook.soloader.SoLoader
