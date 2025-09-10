05b6ca8fc118e47740592bea1378cda5723e69dc
aman-apptile <mohammed.aman@apptile.io>
2025-09-10 16:34:33 +0530

    Fixed missing `notifyJSReady()` function call

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // import {NativeModules} from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { 
-  apptileNavigationRef, 
-  ApptileWrapper, 
-  ApptileAppRoot, 
-  useStartApptile
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {
+  apptileNavigationRef,
+  ApptileWrapper,
+  ApptileAppRoot,
+  useStartApptile,
 } from 'apptile-core';
 
 // import JSSplash from './components/JSSplash';
 import UpdateModal from './components/UpdateModal';
 import AdminPage from './components/AdminPage';
 // import FloatingUpdateModal from './components/FloatingUpdateModal';
+import {NativeModules} from 'react-native';
+const {RNApptile} = NativeModules;
 
 export type ScreenParams = {
   NocodeRoot: undefined;
@@ -29,49 +31,51 @@ function App(): React.JSX.Element {
   const status = useStartApptile(initAnalytics, true);
 
   let body = (
-      <NavigationContainer
-        ref={apptileNavigationRef}
-        theme={{
-          ...DefaultTheme,
-          colors: status.theme
-        }}
-        linking={status.linking}
-      >
-        <Stack.Navigator
-          screenOptions={{
-            animation: 'none',
-          }}
-        >
-          <Stack.Screen 
-            name="NocodeRoot" 
-            component={ApptileAppRoot} 
-            options={{headerShown: false}} 
-          /> 
-          <Stack.Screen 
-            name="NativeUtils" 
-            component={UpdateModal} 
-            options={{headerShown: true}} 
-            initialParams={{appId: status.appId}} 
-          />
-          <Stack.Screen 
-            name="AdminPage" 
-            component={AdminPage} 
-            options={{headerShown: true}}
-            initialParams={{appId: status.appId}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+    <NavigationContainer
+      ref={apptileNavigationRef}
+      theme={{
+        ...DefaultTheme,
+        colors: status.theme,
+      }}
+      linking={status.linking}
+      onReady={() => {
+        RNApptile.notifyJSReady();
+      }}>
+      <Stack.Navigator
+        screenOptions={{
+          animation: 'none',
+        }}>
+        <Stack.Screen
+          name="NocodeRoot"
+          component={ApptileAppRoot}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="NativeUtils"
+          component={UpdateModal}
+          options={{headerShown: true}}
+          initialParams={{appId: status.appId}}
+        />
+        <Stack.Screen
+          name="AdminPage"
+          component={AdminPage}
+          options={{headerShown: true}}
+          initialParams={{appId: status.appId}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 
   // The nocode layer will not do navigation to these screens so you can handle those navigations in the onNavigationEvent
   return (
     <ApptileWrapper
-      noNavigatePaths={["NativeUtils", "AdminPage"]} 
-      onNavigationEvent={(ev) => {
-        console.log("handle navigation event", ev)
-        apptileNavigationRef.current.navigate(ev.screenName, {appId: status.appId});
-      }}
-    >
+      noNavigatePaths={['NativeUtils', 'AdminPage']}
+      onNavigationEvent={ev => {
+        console.log('handle navigation event', ev);
+        apptileNavigationRef.current.navigate(ev.screenName, {
+          appId: status.appId,
+        });
+      }}>
       {body}
     </ApptileWrapper>
   );
