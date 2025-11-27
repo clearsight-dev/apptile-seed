ad0507f67af8499143f991834c5a32c835451826
Ibtesam.Ansari <ibtesam.ansari@apptile.io>
2025-11-27 18:19:49 +0530

    added react native moengage stubs

---
+++ stubs/react-native-moengage/index.ts
@@ -0,0 +1,9 @@
+
+export const MoEProperties = {};
+export const MoEInitConfig = {};
+export const MoEPushConfig = {};
+export const MoEngageLogConfig = {};
+export const MoEngageLogLevel = {};
+export const MoEAppStatus = {};
+export const Platform = {};
+export default {}
