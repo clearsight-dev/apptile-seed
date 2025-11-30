bf0a8ee1b9bf40c019fdd07b150dcd3481f5c06f
Ibtesam.Ansari <ibtesam.ansari@apptile.io>
2025-11-30 08:57:28 +0530

    added js splash

--- components/JSSplash.tsx
+++ components/JSSplash.tsx
@@ -1,14 +1,76 @@
-import React from 'react'
-import {Image, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Platform, View, StyleSheet, Modal, Image, NativeModules} from 'react-native';
 
-export default function JSSplash() {
+const {RNApptile} = NativeModules;
+
+const styles = StyleSheet.create({
+  root: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export let hideJSSplashScreen: () => void;
+export let showJSSplashScreen: () => void;
+
+let isSplashHiddenAtStart = false;
+
+const JSSplash: React.FC<{children?: React.ReactNode; splashImageSource?: any}> = ({
+  children,
+  splashImageSource,
+}) => {
+  const [isRendered, setIsRendered] = useState(false);
+  const [isHidden, setIsHidden] = useState(isSplashHiddenAtStart);
+
+  useEffect(() => {
+    hideJSSplashScreen = () => {
+      setIsHidden(true);
+      RNApptile?.notifyJSReady();
+    };
+    showJSSplashScreen = () => setIsHidden(false);
+    setIsRendered(true);
+
+    return () => {
+      hideJSSplashScreen = () => {};
+    };
+  }, []);
+
+  const imageSource = splashImageSource;
+
   return (
-    <View>
-      <Image 
-        resizeMode={"cover"}
-        style={{width: '100%', height: '100%'}}
-        source={require('../assets/splash.png')} 
-      />
-    </View>
+    <>
+      {isRendered && children}
+      {!isHidden && (
+        <Wrapper>
+          <View style={styles.container}>
+            <Image style={styles.image} source={imageSource} resizeMode="cover" />
+          </View>
+        </Wrapper>
+      )}
+    </>
   );
-}
+};
+
+const Wrapper: React.FC<{children?: React.ReactNode}> = ({children}) => {
+  return Platform.OS === 'android' ? (
+    <Modal statusBarTranslucent transparent>
+      {children}
+    </Modal>
+  ) : (
+    <View style={styles.root}>{children}</View>
+  );
+};
+
+export default JSSplash;
