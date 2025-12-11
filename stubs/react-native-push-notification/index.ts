5b4cf30af847c757111a9f665ce7dea6a9607ecb
mansi10101 <mansi.nagaria@apptile.io>
2025-12-11 17:58:39 +0530

    firebase - onesignal changes and googleservice info file download

--- stubs/react-native-push-notification/index.ts
+++ stubs/react-native-push-notification/index.ts
@@ -1,1 +1,37 @@
-export default {}; // Default stub for react-native-push-notification
+// Stub for react-native-push-notification
+// Firebase Analytics in apptile-core calls PushNotification.configure(), so we need these methods
+
+const PushNotification = {
+  configure: (options: any) => {
+    console.log('[PushNotification Stub] configure() called - using OneSignal instead');
+    if (options && options.onRegister) {
+      options.onRegister({ token: 'stub-token', os: 'ios' });
+    }
+  },
+  localNotification: () => {},
+  localNotificationSchedule: () => {},
+  requestPermissions: () => Promise.resolve(),
+  checkPermissions: (cb: any) => cb && cb({ alert: false, badge: false, sound: false }),
+  cancelLocalNotification: () => {},
+  cancelAllLocalNotifications: () => {},
+  setApplicationIconBadgeNumber: () => {},
+  getApplicationIconBadgeNumber: (cb: any) => cb && cb(0),
+  popInitialNotification: (cb: any) => cb && cb(null),
+  abandonPermissions: () => {},
+  registerNotificationActions: () => {},
+  clearAllNotifications: () => {},
+  removeAllDeliveredNotifications: () => {},
+  getDeliveredNotifications: (cb: any) => cb && cb([]),
+  getScheduledLocalNotifications: (cb: any) => cb && cb([]),
+  removeDeliveredNotifications: () => {},
+  invokeApp: () => {},
+  getChannels: (cb: any) => cb && cb([]),
+  channelExists: (_id: string, cb: any) => cb && cb(false),
+  createChannel: (_ch: any, cb: any) => cb && cb(true),
+  channelBlocked: (_id: string, cb: any) => cb && cb(false),
+  deleteChannel: () => {},
+};
+
+
+
+export default PushNotification;
