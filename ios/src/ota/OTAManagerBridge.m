b46ab5350e670460858b17f597ae0947f7fc06d7
Ibtesam.Ansari <ibtesam.ansari@apptile.io>
2026-01-09 16:45:49 +0530

    native ota

---
+++ ios/src/ota/OTAManagerBridge.m
@@ -0,0 +1,15 @@
+//
+//  OTAManagerBridge.m
+//  apptileSeed
+//
+//  Objective-C bridge for OTAManager Swift module.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(OTAManager, NSObject)
+
+RCT_EXTERN_METHOD(restartApp)
+
+@end
+
